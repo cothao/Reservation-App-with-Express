32ea9d28532b90ce6d786834734a24de8e9a1323
cothao <114521616+cothao@users.noreply.github.com>
2023-05-31 18:26:25 -0500

    added logins and logouts

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const createUser = document.querySelector('#create-user-form')
 const firstName = document.querySelector('#firstName')
 const lastName = document.querySelector("#lastName");
 const userPassword = document.querySelector('#password')
+const greetingName = document.querySelector('#username')
+const logout = document.querySelector('#logout')
 
 async function logJSONData() {
   const response = await fetch("http://localhost:5500/data");
@@ -10,26 +12,40 @@ async function logJSONData() {
   return jsonData
 }
 
+let currentUser = localStorage.getItem('currentUser')
+window.addEventListener('load', () => {
+  if (
+    currentUser &&
+    window.location.href === "http://127.0.0.1:5501/src/signin.html"
+  ) {
+    window.location.replace("http://127.0.0.1:5501/src/reservationPage.html");
+  }
+}, false)
+
 window.addEventListener('DOMContentLoaded', () => {
   logJSONData()
+  console.log(currentUser)
 })
 
 // myForm.addEventListener("submit", async (e) => {
 //   e.preventDefault()
 // });
 //2944.52
+// LOGIN FORM
 myForm?.addEventListener('submit', async(e) => {
   e.preventDefault()
   
   let uso = await logJSONData() /* this function returns json data that
   we await for, we then await the data in this function call so that it may return */
   
-  let user = uso.result.find(data => {
+  let user = uso.result?.find(data => {
     return (data.Email === e.target[0].value) && data.Password === e.target[1].value
   })
   if (user) {
     e.target.submit()
-    console.log(e.currentTarget)
+    localStorage.setItem('currentUser', JSON.stringify(user))
+  } else {
+    alert('Email or Password is incorrect')
   }
 })
 
@@ -53,6 +69,12 @@ createUser?.addEventListener('submit', (e) => {
     return data
   }) // like above, this is why we put it in json format; then grabs a fulfilled promise
 })
+
+logout.addEventListener('click', () => {
+  localStorage.clear()
+})
+
+greetingName.textContent = `Hello ${JSON.parse(currentUser).Firstname}!`
 
 class Employee {
   constructor(EmployeeID, Firstname, lastname, Age, Gender) {
@@ -79,7 +101,5 @@ const getData = async (url) => {
   }).then((res) => res.json()); // like above, this is why we put it in json format; then grabs a fulfilled promise
   console.log(newData); // then we are console logging it
 };
-
-  
 
 getData("/"); // we call the function with the root as the parameter
